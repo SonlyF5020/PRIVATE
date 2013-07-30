60be086c50879a39659bcecef89aa3b4ced41976
SonlyF5020 <502089948@163.com>
2013-07-30 18:45:07 +0800

    second test

--- src/main/java/JustForTest.java
+++ src/main/java/JustForTest.java
@@ -2,5 +2,8 @@
 public class JustForTest {
     public static void main(String[] args){
         System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
+        System.out.println("hello World");
     }
 }
